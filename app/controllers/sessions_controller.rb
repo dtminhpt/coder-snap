23cb3d413ca5e47ef550346cca3da081164cbbc1
Minh Dinh <dtminhpt@gmail.com>
2016-03-18 21:21:55 +0700

    Adding logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out."
+    redirect_to root_path
+  end
 end
