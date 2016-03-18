79280496d2b4f8c82680a98d09ed8a2026abac02
Minh Dinh <dtminhpt@gmail.com>
2016-03-18 23:35:57 +0700

    Adding login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,14 @@ class SessionsController < ApplicationController
     flash[:success] = "Logged out."
     redirect_to root_path
   end
+
+  def create 
+    if @user = User.find_by(email: params[:email]) and @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, flash: {success: "Logged in"}
+    else
+      flash.now[:error] = "Invalid username or password"
+      render 'new'
+    end
+  end 
 end
