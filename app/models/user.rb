16025c372ac09b4c3114333975577ffb671b4987
Minh Dinh <dtminhpt@gmail.com>
2016-03-19 00:32:11 +0700

    Adding more validates for name, mail and password in SignUp page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password 
 
   validates :email, uniqueness: true
-  validates :name, presence: true
+  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :name, :email, :password, presence: true
+
+  
 end
