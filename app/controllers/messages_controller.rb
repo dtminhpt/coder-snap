b31ea021098ea1f75e954de4805d00220b8543a6
Minh Dinh <dtminhpt@gmail.com>
2016-03-19 16:24:53 +0700

    Create POST method 'create' for messagescontroller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,16 @@ class MessagesController < ApplicationController
     @message = current_user.sent_messages.new
   end
 
+  
+  # POST /messages
   def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to sent_messages_path, notice: 'Message was successfully created.'
+    else
+      render :new
+    end
   end
 
   def index
@@ -18,4 +27,11 @@ class MessagesController < ApplicationController
 
   def sent
   end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def message_params
+      params.require(:message).permit(:sender_id, :recipient_id, :content, :read_at)
+    end
 end
