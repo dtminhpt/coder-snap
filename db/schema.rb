4d7e80c665fc1ab6aa965d460edb2a7e83d8d0b0
Minh Dinh <dtminhpt@gmail.com>
2016-03-19 15:03:04 +0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318171443) do
+ActiveRecord::Schema.define(version: 20160319075425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
